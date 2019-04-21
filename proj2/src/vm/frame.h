b59863a8c5aa27eaea4c1eff934e3bd76e9e1906
cheses100 <dmizrahi@usc.edu>
2019-04-20 17:14:41 -0700

    Updated additional files

--- proj2/src/vm/frame.h
+++ proj2/src/vm/frame.h
@@ -1,7 +1,14 @@
+#include "page.h"
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <list.h>
+
 struct list frame_table;
 
 struct frame_table_entry {
 	uint32_t* frame;
 	struct thread* owner;
 	struct sup_page_table_entry* aux;
-}
+};
--- proj2/src/vm/page.h
+++ proj2/src/vm/page.h
@@ -1,3 +1,13 @@
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+
 struct sup_page_table_entry {
 	uint32_t* uservaddr;
-}
+	uint64_t access_time;
+
+	bool dirty;
+	bool accessed;
+};
